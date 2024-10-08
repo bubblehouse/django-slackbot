afbf58394a4cb10abcb28488c3e00f1b9febebe2
Phil Christensen <phil.christensen@eaze.com>
2024-10-08 14:16:47 -0400

    fix: typo

--- django_slackbot/__init__.py
+++ django_slackbot/__init__.py
@@ -4,4 +4,4 @@ __version__ = "1.0.7"
 def get_version():
     return __version__
 
-default_app_config = 'django_slackbot.apps.DjangoSlackConfig'
+default_app_config = 'django_slackbot.apps.DjangoSlackBotConfig'
